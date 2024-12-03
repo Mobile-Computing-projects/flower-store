3bbba2ecf7080b517eeac94e530a7573b3bce98f
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2024-12-03 23:38:08 +0530

    fix: eslint warnings

--- myapp/src/components/Product.js
+++ myapp/src/components/Product.js
@@ -1,5 +1,4 @@
 import '../assets/CSS/layout.css';
-import {flowers} from './FlowerDB';
 import {useState} from "react";
 
 export default function Product({setCart,setTotal,flower}){
@@ -27,7 +26,7 @@ export default function Product({setCart,setTotal,flower}){
         <div className="grid-item">
 
             <div class="card">
-            <img  src={require("../assets/image/" + flower.img)}  />
+            <img  src={require("../assets/image/" + flower.img)} alt={flower.name}  />
                 <div class="card-body">
                     <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
                     <div class="quantity-container">
